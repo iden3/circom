92b9f95f481dcb1bb0cb83dd772171939bb97c35
Victor Carrillo Redondo <victcarr@ucm.es>
2022-09-09 10:57:52 +0200

    getting information about the usage of custom templates from ProgramArchive

--- circom/src/execution_user.rs
+++ circom/src/execution_user.rs
@@ -40,6 +40,7 @@ pub fn execute_project(
         flag_old_heuristics: config.flag_old_heuristics,
         prime : config.prime,
     };
+    let custom_gates = program_archive.custom_gates;
     let (exporter, vcp) = build_circuit(program_archive, build_config)?;
     if config.r1cs_flag {
         generate_output_r1cs(&config.r1cs, exporter.as_ref(), custom_gates)?;
