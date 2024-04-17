e55d0409c92fe1c296821e1a56e2851046e31a4e
alrubio <alberu04@ucm.es>
2024-04-17 07:28:48 +0200

    hash search fixed

--- code_producers/src/c_elements/common/calcwit.cpp
+++ code_producers/src/c_elements/common/calcwit.cpp
@@ -52,10 +52,10 @@ uint Circom_CalcWit::getInputSignalHashPosition(u64 h) {
   uint pos = (uint)(h % (u64)n);
   if (circuit->InputHashMap[pos].hash!=h){
     uint inipos = pos;
-    pos++;
+    pos = (pos+1)%n; 
     while (pos != inipos) {
-      if (circuit->InputHashMap[pos].hash==h) return pos;
-      if (circuit->InputHashMap[pos].hash==0) {
+      if (circuit->InputHashMap[pos].hash == h) return pos;
+      if (circuit->InputHashMap[pos].signalid == 0) {
 	fprintf(stderr, "Signal not found\n");
 	assert(false);
       }
