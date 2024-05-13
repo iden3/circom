ca7fc5e991b8e3679870495b73f98719d448f72c
Andrew Morris <voltrevo@gmail.com>
2024-05-14 08:32:30 +1000

    Remove vfs_utils

--- vfs_utils/src/main.rs
+++
@@ -1,6 +0,0 @@
-use vfs_utils::{canonicalize_physical_path, normalize_physical_path};
-
-fn main() {
-    println!("{}", canonicalize_physical_path("."));
-    println!("{}", normalize_physical_path("foo/bar"));
-}
