69cea76e623d5cfc9e48517eb287f1888ae4e92b
clararod9 <clarrodr@ucm.es>
2025-03-24 20:47:07 +0100

    solving minor multiple assignments analysis

--- constraint_generation/src/execute.rs
+++ constraint_generation/src/execute.rs
@@ -3148,12 +3148,15 @@ fn execute_function_call(
     runtime: &mut RuntimeInformation,
     flags: FlagsExecution
 ) -> Result<(FoldedValue, bool), ()> {
+    use std::mem;
     let previous_block = runtime.block_type;
+    let previous_conditions = mem::replace(&mut runtime.conditions_state, vec![]);
     runtime.block_type = BlockType::Known;
     let function_body = program_archive.get_function_data(id).get_body_as_vec();
     let (function_result, can_be_simplified) =
         execute_sequence_of_statements(function_body, program_archive, runtime, &mut Option::None, flags, true)?;
     runtime.block_type = previous_block;
+    runtime.conditions_state = previous_conditions;
     let return_value = function_result.unwrap();
     debug_assert!(FoldedValue::valid_arithmetic_slice(&return_value));
     Result::Ok((return_value, can_be_simplified))
