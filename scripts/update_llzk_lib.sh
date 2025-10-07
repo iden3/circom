52a339e98e45c37b8815cb3bfcc132c3c10d5e2d
Timothy Hoffman <4001421+tim-hoffman@users.noreply.github.com>
2025-10-07 15:44:55 -0500

    update llvm dependencies and add script to do that easily (#150)

---
+++ scripts/update_llzk_lib.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Program: update_llzk_lib.sh
+# Description: This script updates the llzk-lib dependency
+#   in the cargo lock file and the nix flake lock file.
+#
+# Required Programs:
+#   - cargo: For updating the rust dependencies
+#   - nix: For updating the nix flake
+#
+# Usage: ./scripts/update_llzk_lib.sh
+
+set -e
+
+cargo update -p llzk-sys
+cargo update -p llzk
+nix flake update llzk-lib
+nix flake update llzk-rs-pkgs
